ecce7f5f2c6fbe81938c4ff64a3a5f3a711044e6
咕咕波仔 <94773509+fisherpine@users.noreply.github.com>
2023-06-14 17:27:11 +0800

    登录接口（上），POST请求测试成功与失败

--- src/main/java/com/ggbz/sys/controller/UserController.java
+++ src/main/java/com/ggbz/sys/controller/UserController.java
@@ -4,11 +4,10 @@ import com.ggbz.common.vo.Result;
 import com.ggbz.sys.entity.User;
 import com.ggbz.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -31,5 +30,14 @@ public class UserController {
     public Result<List<User>> getAllUser(){
         List<User> list = userService.list();
         return Result.success(list,"查询成功");
+    }
+
+    @PostMapping("/login")
+    public Result<Map<String,Object>> login(@RequestBody User user){
+        Map<String,Object> data = userService.login(user);
+        if(data!=null) {
+            return Result.success(data);
+        }
+        return Result.fail(20002,"用户名或密码错误");
     }
 }
--- src/main/java/com/ggbz/sys/service/impl/UserServiceImpl.java
+++ src/main/java/com/ggbz/sys/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.ggbz.sys.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ggbz.sys.entity.User;
 import com.ggbz.sys.mapper.UserMapper;
 import com.ggbz.sys.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * <p>
@@ -17,4 +22,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Map<String, Object> login(User user) {
+        //根据用户名和密码查询
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getUsername,user.getUsername());
+        wrapper.eq(User::getPassword,user.getPassword());
+        User loginUser = this.baseMapper.selectOne(wrapper);
+        //结果不为空，则生成token，并将用户信息存入redis
+        if(loginUser != null){
+            //暂时用UUID，中级方案是jwt
+            String key = "user:" + UUID.randomUUID();
+
+            //存入redis
+
+            //返回数据
+            Map<String,Object> data = new HashMap<>();
+            data.put("token",key);
+            return data;
+        }
+        return null;
+    }
 }
